963373f66250a17813b582fe6c32865b90ba8d0e
acer <acer@DESKTOP-DFUB04I>
2023-05-18 08:12:46 -0700

    mange a book

---
+++ src/grup/m/bse203083/bse203070/GrupMBSE203083BSE203070.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package grup.m.bse203083.bse203070;
+
+/**
+ *
+ * @author acer
+ */
+public class GrupMBSE203083BSE203070 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
