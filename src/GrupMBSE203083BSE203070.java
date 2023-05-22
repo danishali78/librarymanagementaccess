18273704049ed98a0028d884a0b34b103c309c9d
HP <HP@DESKTOP-5DQ7SR5>
2023-05-22 14:03:28 +0500

    functionality updated of admin page

--- src/GrupMBSE203083BSE203070.java
+++ src/GrupMBSE203083BSE203070.java
@@ -88,16 +88,7 @@ private JPasswordField passwordField;
                 loginGUI.setVisible(true);
             }
         });
-         Book book = new Book(23456,"truth","gener","1234");
-         User user = new User(203083,"ali","233344555");
-         BiookDoa obj = new Bookdaoimpl();
-         obj.returnbook(4523);
-         obj.addBook(book);
-         obj.issuebook(25342);
-         obj.getIssuedBooks();
-         Userrdoa obj1 = new userdoa();
-         obj1.getalluser();
-         obj1.adduser(user);
+        
     }
 }
 
@@ -107,51 +98,106 @@ private DefaultListModel<String> listModel;
 public BiookDoa bookDAO;
 public Userrdoa userDAO;
     public AdminFunctionPageGUI() {
+         bookDAO = new Bookdaoimpl(); 
+        userDAO = new userdoa();
         setTitle("Admin Function Page");
         JLabel tf = new JLabel("Admin functions ");
         tf.setBounds(110, 50, 100, 30);
         
         setSize(600, 500);
         JButton bt = new JButton("View book");
-
-       
-        
+        bt.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                viewBooks();
+            }
+        });
         bt.setBounds(110, 140, 100, 30);
         bt.setBackground(Color.GRAY);
         JButton bt1 = new JButton("view user");
-         
+         bt1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getallusers();
+            }
+        });
         bt1.setBounds(220, 140, 100, 30);
         bt1.setBackground(Color.GRAY);
         JButton bt2 = new JButton("view issue book");
-        
+        bt2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                getIssuedBooks();
+            }
+        });
         bt2.setBounds(330, 140, 100, 30);
         bt2.setBackground(Color.GRAY);
          JButton bt3 = new JButton("issue book");
            bt3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                openIssueBook();
+                String bookIdString = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter the book ID:", "Issue Book", JOptionPane.PLAIN_MESSAGE);
+        if (bookIdString != null) {
+            try {
+                int bookId = Integer.parseInt(bookIdString);
+                issuebook(bookId);
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(AdminFunctionPageGUI.this, "Invalid book ID.", "Error", JOptionPane.ERROR_MESSAGE);
             }
-        });
+        }
+    }
+});
            
         bt3.setBounds(440, 140, 100, 30);
         bt3.setBackground(Color.GRAY);
         JButton bt4 = new JButton("add user"); 
         bt4.setBounds(110, 200, 100, 30);
         bt4.setBackground(Color.GRAY);
+         bt4.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        String id1 = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter user id:", "Add User", JOptionPane.PLAIN_MESSAGE);
+        String name = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter user name :", "Add User", JOptionPane.PLAIN_MESSAGE);
+        String password = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter user password:", "Add User", JOptionPane.PLAIN_MESSAGE);
+int id=Integer.parseInt(id1);
+            User user = new User(id, name, password);
+            adduser(user);
+        
+    }
+});
         JButton bt5 = new JButton("add book");
            bt5.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                openaddBook();
-            }
-        });
+    public void actionPerformed(ActionEvent e) {
+        String id1 = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter book id:", "Add Book", JOptionPane.PLAIN_MESSAGE);
+        String title = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter book title:", "Add Book", JOptionPane.PLAIN_MESSAGE);
+        String genre = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter book genre:", "Add Book", JOptionPane.PLAIN_MESSAGE);
+        String price = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter book price:", "Add Book", JOptionPane.PLAIN_MESSAGE);
+        int id=Integer.parseInt(id1);
+            Book book = new Book(id,title, genre, price);
+            addBook(book);
+        
+    }
+});
         bt5.setBounds(220, 200, 100, 30);
         bt5.setBackground(Color.GRAY);
         JButton bt6 = new JButton("return book");
-        
+       bt6.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        String bookIdString = JOptionPane.showInputDialog(AdminFunctionPageGUI.this, "Enter the book ID:", "Return Book", JOptionPane.PLAIN_MESSAGE);
+        if (bookIdString != null) {
+            try {
+                int bookId = Integer.parseInt(bookIdString);
+                returnbook(bookId);
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(AdminFunctionPageGUI.this, "Invalid book ID.", "Error", JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+});
         bt6.setBounds(330, 200, 100, 30);
         bt6.setBackground(Color.GRAY);
         
-        JButton bt7 = new JButton("create/reset");
+        JButton bt7 = new JButton("create database");
+          bt7.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        createdatabase();
+    }
+});
         bt7.setBounds(440, 200, 100, 30);
         bt7.setBackground(Color.GRAY);
         setLayout(null);
@@ -167,119 +213,84 @@ public Userrdoa userDAO;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
     }
-       private void openIssueBook()
-       {
-        // Close the admin page
-        dispose();
+private void viewBooks() {
+        List<Book> books = bookDAO.ViewBooks();
 
-        // Open the next page
-        IssueBook nextPageGUI = new IssueBook();
-        nextPageGUI.setVisible(true);
+        StringBuilder sb = new StringBuilder();
+        for (Book book : books) {
+            sb.append("ID: ").append(book.getId()).append(", ")
+              .append("Title: ").append(book.getTitle()).append(", ")
+              .append("Genre: ").append(book.getgenre()).append(", ")
+              .append("Price: ").append(book.getprice()).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(this, sb.toString(), "View Books", JOptionPane.PLAIN_MESSAGE);
     }
-       
-        private void openaddBook()
-       {
-        // Close the admin page
-        dispose();
 
-        // Open the next page
-        addBook object = new  addBook();
-        object.setVisible(true);
-    }
-}  
+    private void getallusers() {
+       List<User> users = userDAO.getalluser();
 
-class IssueBook extends JFrame {
-   
-    public IssueBook()
-    {
-        setTitle("Next Page");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-        JLabel tf = new JLabel("please fill these form  ");
-        
-        JLabel BookID = new JLabel("Book ID");
-        BookID.setBounds(50, 30, 80, 25);
-        
-        JLabel Userid = new JLabel("User ID");
-        Userid.setBounds(50, 80, 80, 25);
-        
-        JLabel period = new JLabel("Period");
-        period.setBounds(50, 120, 80, 25);
-        
-        JTextField bookidfield = new JTextField();
-        bookidfield.setBounds(50, 50, 120, 25);
-        
-        JTextField userid = new JTextField();
-        userid.setBounds(50, 100, 120, 25);
-        
-        JTextField periodfield = new JTextField();
-        periodfield.setBounds(50, 150, 120, 25);
-        
-        JButton Button = new JButton("submit");
-        Button.setBounds(110, 200, 80, 25);
-        
-        setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
-        add(BookID);
-        add(Userid);
-        add(period);
-        add(bookidfield);
-        add(userid);
-        add(periodfield);
-        add(Button);
-        add(panel);
+    if (!users.isEmpty()) {
+        StringBuilder sb = new StringBuilder();
+        for (User user : users) {
+            sb.append("ID: ").append(user.getuid()).append(", ")
+              .append("Name: ").append(user.getusername()).append(", ")
+              .append("Password: ").append(user.getpassword()).append("\n");
+        }
+
+        JTextArea textArea = new JTextArea(sb.toString());
+        textArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
+        JOptionPane.showMessageDialog(this, scrollPane, "View Users", JOptionPane.PLAIN_MESSAGE);
+    } else {
+        JOptionPane.showMessageDialog(this, "No users found.", "View Users", JOptionPane.INFORMATION_MESSAGE);
     }
 }
 
- class addBook extends JFrame{
-     
-     public addBook(Book book)
-     {
-         
-        setTitle("add book");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(400, 300);
-        JLabel tf = new JLabel("please fill these form  ");
-        
-       JLabel Bookname = new JLabel("Book name");
-        Bookname.setBounds(50, 30, 80, 25);
-        
-         JLabel Bookid = new JLabel("Book name");
-        Bookid.setBounds(50, 30, 80, 25);
-        
-       JLabel genre = new JLabel("Genre");
-        genre.setBounds(50, 80, 80, 25);
-        
-       JLabel price = new JLabel("Price");
-        price.setBounds(50, 120, 80, 25);
-                
-        JTextField bookname = new JTextField();
-        bookname.setBounds(50, 50, 120, 25);
-        
-        JTextField bookid = new JTextField();
-        bookid.setBounds(50, 100, 120, 25);
-        
-        JTextField genrefield = new JTextField();
-        genrefield.setBounds(50, 150, 120, 25);
-        
-        JTextField pricefield = new JTextField();
-        pricefield.setBounds(50, 200, 120, 25);
-        
-       JButton Button = new JButton("submit");
-        Button.setBounds(110, 200, 80, 25);
-        
-        setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
-        add(Bookname);
-        add(genre);
-        add(price);
-        add(bookname);
-        add(genrefield);
-        add(pricefield);
-        add(Button);
-        add(panel);
-        addBook obj = new addBook(book);
-     }
+    private void getIssuedBooks() {
+        List<Book> issuedBooks = bookDAO.getIssuedBooks();
 
-    
+        StringBuilder sb = new StringBuilder();
+        for (Book book : issuedBooks) {
+            sb.append("ID: ").append(book.getId()).append(", ")
+              .append("Title: ").append(book.getTitle()).append(", ")
+              .append("Genre: ").append(book.getgenre()).append(", ")
+              .append("Price: ").append(book.getprice()).append("\n");
+        }
+
+        JOptionPane.showMessageDialog(this, sb.toString(), "View Issued Books", JOptionPane.PLAIN_MESSAGE);
+    }
+
+    private void issuebook(int bookId) {
+       bookDAO.issuebook(bookId);
+    JOptionPane.showMessageDialog(this, "Book issued successfully.", "Issue Book", JOptionPane.INFORMATION_MESSAGE);
 }
+
+    private void adduser(User user) {
+        userDAO.adduser(user);
+    JOptionPane.showMessageDialog(this, "User added successfully.", "Add User", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void addBook(Book book) {
+        bookDAO.addBook(book);
+    JOptionPane.showMessageDialog(this, "Book added successfully.", "Add Book", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void returnbook(int bookId) {
+        bookDAO.returnbook(bookId);
+    JOptionPane.showMessageDialog(this, "Book returned successfully.", "Return Book", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    private void createdatabase() {
+         bookDAO=new Bookdaoimpl();
+
+    JOptionPane.showMessageDialog(this, "Database created/ successfully.", "Create Database", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public static void main(String[] args) {
+        AdminFunctionPageGUI adminGUI = new AdminFunctionPageGUI();
+        adminGUI.setVisible(true);
+    }
+}
+
